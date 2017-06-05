16198a1ecb1d1336dff46c9223443182c80b556f
Tim Daley <tim.daley@cru.org>
2017-06-05 13:40:31 -0400

    Update install-pip.sh

--- install-pip.sh
+++ install-pip.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-# Install PIP by running the following:  
+# Install PIP by running the following:
+echo "Installing pip:"
 easy_install --user pip
 
-# Add python to the path by running the following:  
+# Add python to the path by running the following:
+echo "Installing python to the path"
 printf 'if [ -f ~/.bashrc ]; then\n  source ~/.bashrc\nfi\n' >> $HOME/.profile  
 printf 'export PATH=$PATH:$HOME/Library/Python/2.7/bin\n' >> $HOME/.bashrc  
 source $HOME/.profile
 
-# Verify pip is running by running the following:  
+# Verify pip is running by running the following:
+echo "Verifying pip:"
 pip --version
+
+# please run the following command or restart your shell
+source $HOME/.profile
