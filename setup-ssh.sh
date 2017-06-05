c8d23600987d978788d065e521ce065d8042da1a
Tim Daley <tim.daley@cru.org>
2017-06-05 14:08:53 -0400

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -9,7 +9,7 @@ if [[ ! -e ~root/.ssh ]]; then
   sudo chown root:wheel ~root/.ssh
   sudo chmod 700 ~root/.ssh
 fi
-if [[ ! -e ~/.ssh/id_rsa ]]
+if [[ ! -e ~/.ssh/id_rsa ]]; then
   ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ''
 fi
 cat ~/.ssh/id-rsa.pub sudo 'cat >>~/.ssh/authorized_keys'
