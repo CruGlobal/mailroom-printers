21f03994eab47f9fa1043069a818323553baff6d
Tim Daley <tim.daley@cru.org>
2017-06-05 14:26:39 -0400

    Update setup-ssh.sh

--- setup-ssh.sh
+++ setup-ssh.sh
@@ -1,17 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
-if [[ ! -e .ssh ]]; then  
+if [ ! -d ~/.ssh ]; then  
   sudo mkdir ~/.ssh
   sudo chmod 700 ~/.ssh
 fi
-if [[ ! -e ~root/.ssh ]]; then  
+if [ ! -d ~root/.ssh ]; then  
   sudo mkdir ~root/.ssh
   sudo chown root:wheel ~root/.ssh
   sudo chmod 700 ~root/.ssh
 fi
-if [[ ! -e ~/.ssh/id_rsa ]]; then
+if [ ! -e ~/.ssh/id_rsa ]; then
   ssh-keygen -t rsa -q -f ~/.ssh/id_rsa -P ''
 fi
-cat ~/.ssh/id-rsa.pub sudo 'cat >>~/.ssh/authorized_keys'
+
+sudo touch ~root/.ssh/authorized_keys
 sudo chown root:wheel ~root/.ssh/authorized_keys
-sudo chmod 600 ~root.ssh/authorized_keys
+sudo chmod 600 ~root/.ssh/authorized_keys
+
+U=${USER}
+sudo sh -c "cat ~${U}/.ssh/id_rsa.pub >>~root/.ssh/authorized_keys"
