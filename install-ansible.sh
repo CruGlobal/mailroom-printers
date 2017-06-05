78de48c28fdeff591477656fca588954b21aed62
Tim Daley <tim.daley@cru.org>
2017-06-05 13:45:53 -0400

    Update install-ansible.sh

--- install-ansible.sh
+++ install-ansible.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 # Install ansible via PIP by running:
+echo ""
 echo "Installing ansible:"
 pip install --user --upgrade ansible
 
 # Verify ansible by running the following:
+echo ""
 echo "Verifying ansible installation:"
 type -a ansible
 ansible --version
 
 # Create the system-wide Ansible directory.
+echo ""
 echo "Creating system-wide Ansible directory:"
 sudo mkdir /etc/ansible
 
 # Copy the default Ansible configuration file to the system-wide Ansible directory.
+echo ""
 echo "Copying Ansible configuration to system-wide Ansible directory:"
 sudo curl -L https://raw.githubusercontent.com/ansible/ansible/devel/examples/ansible.cfg -o /etc/ansible/ansible.cfg
 
